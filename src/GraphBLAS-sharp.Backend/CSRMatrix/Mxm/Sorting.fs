d223cb70fbe0b2e0a7577cfe5db86235f482b15c
unknown <artem_chernikov00@list.ru>
2021-12-03 13:17:43 +0300

    Optimized sorting

--- src/GraphBLAS-sharp.Backend/CSRMatrix/Mxm/Sorting.fs
+++ src/GraphBLAS-sharp.Backend/CSRMatrix/Mxm/Sorting.fs
@@ -2,54 +2,120 @@ namespace GraphBLAS.FSharp.Backend
 
 open Brahma.FSharp.OpenCL
 open GraphBLAS.FSharp.Backend
+open GraphBLAS.FSharp.Backend.Common
 
-module internal Sorting =
+module internal rec Sorting =
     let runInPlace (clContext: ClContext) workGroupSize =
         fun (processor: MailboxProcessor<_>) (matrix: CSRMatrix<'a>) ->
-            let sort =
-                <@
-                    fun (ndRange: Range1D)
-                        (rowPointersBuffer: ClArray<int>)
-                        (columnsBuffer: ClArray<int>)
-                        (valuesBuffer: ClArray<'a>) ->
+            let matrixCOO = Converter.toCOO clContext processor workGroupSize matrix
 
-                        let i = ndRange.GlobalID0
-                        let localID = ndRange.LocalID0
-                        let workGroupNumber = i / workGroupSize
+            let packedIndices = pack clContext workGroupSize processor matrixCOO.Rows matrixCOO.Columns
 
-                        let beginIndex = rowPointersBuffer.[workGroupNumber]
-                        let endIndex = rowPointersBuffer.[workGroupNumber + 1]
-                        let mutable n = endIndex - beginIndex
+            RadixSort.sortByKeyInPlace clContext workGroupSize processor packedIndices matrix.Values 2
 
-                        while n > 1 do
-                            for j in beginIndex .. beginIndex + n - 2 do
-                                if columnsBuffer.[j] > columnsBuffer.[j + 1]
-                                then
-                                    let tmp = columnsBuffer.[j]
-                                    columnsBuffer.[j] <- columnsBuffer.[j + 1]
-                                    columnsBuffer.[j + 1] <- tmp
-                                    let tmpValue = valuesBuffer.[j]
-                                    valuesBuffer.[j] <- valuesBuffer.[j + 1]
-                                    valuesBuffer.[j + 1] <- tmpValue
-                            n <- n - 1
-                @>
+            let _, sortedColumns = unpack clContext workGroupSize processor packedIndices
 
-            let kernel = clContext.CreateClKernel(sort)
+            { RowCount = matrix.RowCount
+              ColumnCount = matrix.ColumnCount
+              RowPointers = matrix.RowPointers
+              Columns = sortedColumns
+              Values = matrix.Values }
 
-            let ndRange = Range1D(workGroupSize * matrix.RowCount)
+    let private pack
+        (clContext: ClContext)
+        workGroupSize
+        (processor: MailboxProcessor<_>)
+        (firstArray: ClArray<int>)
+        (secondArray: ClArray<int>) =
 
-            processor.Post(
-                Msg.MsgSetArguments
-                    (fun () ->
-                        kernel.ArgumentsSetter
-                            ndRange
-                            matrix.RowPointers
-                            matrix.Columns
-                            matrix.Values)
+        let length = firstArray.Length
+
+        let pack =
+            <@
+                fun (ndRange: Range1D)
+                    (firstArray: ClArray<int>)
+                    (secondArray: ClArray<int>)
+                    (outputArray: ClArray<uint64>) ->
+
+                    let i = ndRange.GlobalID0
+
+                    if i < length then
+                        outputArray.[i] <- ((uint64 firstArray.[i]) <<< 32) ||| (uint64 secondArray.[i])
+            @>
+
+        let outputArray =
+            clContext.CreateClArray(
+                length,
+                hostAccessMode = HostAccessMode.NotAccessible
             )
 
-    // let private pack
-    //     (clContext: ClContext)
-    //     workGroupSize
-    //     (processor: MailboxProcessor<_>)
-    //     () =
+        let kernel = clContext.CreateClKernel(pack)
+
+        let ndRange = Range1D.CreateValid(length, workGroupSize)
+
+        processor.Post(
+            Msg.MsgSetArguments
+                (fun () ->
+                    kernel.ArgumentsSetter
+                        ndRange
+                        firstArray
+                        secondArray
+                        outputArray)
+        )
+
+        processor.Post(Msg.CreateRunMsg<_, _> kernel)
+
+        outputArray
+
+    let private unpack
+        (clContext: ClContext)
+        workGroupSize
+        (processor: MailboxProcessor<_>)
+        (inputArray: ClArray<uint64>) =
+
+        let length = inputArray.Length
+
+        let pack =
+            <@
+                fun (ndRange: Range1D)
+                    (inputArray: ClArray<uint64>)
+                    (firstArray: ClArray<int>)
+                    (secondArray: ClArray<int>) ->
+
+                    let i = ndRange.GlobalID0
+
+                    if i < length then
+                        let n = inputArray.[i]
+                        firstArray.[i] <- int (n >>> 32)
+                        secondArray.[i] <- int n
+            @>
+
+        let firstArray =
+            clContext.CreateClArray(
+                length,
+                hostAccessMode = HostAccessMode.NotAccessible
+            )
+
+        let secondArray =
+            clContext.CreateClArray(
+                length,
+                hostAccessMode = HostAccessMode.NotAccessible
+            )
+
+        let kernel = clContext.CreateClKernel(pack)
+
+        let ndRange = Range1D.CreateValid(length, workGroupSize)
+
+        processor.Post(
+            Msg.MsgSetArguments
+                (fun () ->
+                    kernel.ArgumentsSetter
+                        ndRange
+                        inputArray
+                        firstArray
+                        secondArray)
+        )
+
+        processor.Post(Msg.CreateRunMsg<_, _> kernel)
+
+        firstArray, secondArray
