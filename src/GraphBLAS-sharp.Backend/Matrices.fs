dee0082d0f51bcc8d492a9e8d326536509b9d475
gsv <rsdpisuy@gmail.com>
2021-12-14 17:53:02 +0300

    Formatting fixed.

--- src/GraphBLAS-sharp.Backend/Matrices.fs
+++ src/GraphBLAS-sharp.Backend/Matrices.fs
@@ -3,34 +3,34 @@ namespace GraphBLAS.FSharp.Backend
 open Brahma.FSharp.OpenCL
 
 type CSRMatrix<'elem when 'elem: struct> =
-    { Context: ClContext      
+    { Context: ClContext
       RowCount: int
       ColumnCount: int
       RowPointers: ClArray<int>
       Columns: ClArray<int>
       Values: ClArray<'elem> }
 
-      interface System.IDisposable with 
-        member this.Dispose() = 
-          let q = this.Context.Provider.CommandQueue
-          q.Post(Msg.CreateFreeMsg<_>(this.Values))
-          q.Post(Msg.CreateFreeMsg<_>(this.Columns))
-          q.Post(Msg.CreateFreeMsg<_>(this.RowPointers))
-          q.PostAndReply(Msg.MsgNotifyMe)
-           
+    interface System.IDisposable with
+        member this.Dispose() =
+            let q = this.Context.Provider.CommandQueue
+            q.Post(Msg.CreateFreeMsg<_>(this.Values))
+            q.Post(Msg.CreateFreeMsg<_>(this.Columns))
+            q.Post(Msg.CreateFreeMsg<_>(this.RowPointers))
+            q.PostAndReply(Msg.MsgNotifyMe)
+
 type TupleMatrix<'elem when 'elem: struct> =
     { Context: ClContext
       RowIndices: ClArray<int>
       ColumnIndices: ClArray<int>
       Values: ClArray<'elem> }
-    
-    interface System.IDisposable with 
-        member this.Dispose() = 
-          let q = this.Context.Provider.CommandQueue
-          q.Post(Msg.CreateFreeMsg<_>(this.RowIndices))
-          q.Post(Msg.CreateFreeMsg<_>(this.ColumnIndices))
-          q.Post(Msg.CreateFreeMsg<_>(this.Values))
-          q.PostAndReply(Msg.MsgNotifyMe)
+
+    interface System.IDisposable with
+        member this.Dispose() =
+            let q = this.Context.Provider.CommandQueue
+            q.Post(Msg.CreateFreeMsg<_>(this.RowIndices))
+            q.Post(Msg.CreateFreeMsg<_>(this.ColumnIndices))
+            q.Post(Msg.CreateFreeMsg<_>(this.Values))
+            q.PostAndReply(Msg.MsgNotifyMe)
 
 type COOMatrix<'elem when 'elem: struct> =
     { Context: ClContext
@@ -39,12 +39,11 @@ type COOMatrix<'elem when 'elem: struct> =
       Rows: ClArray<int>
       Columns: ClArray<int>
       Values: ClArray<'elem> }
-    
-    interface System.IDisposable with 
-        member this.Dispose() = 
-          let q = this.Context.Provider.CommandQueue
-          q.Post(Msg.CreateFreeMsg<_>(this.Values))
-          q.Post(Msg.CreateFreeMsg<_>(this.Columns))
-          q.Post(Msg.CreateFreeMsg<_>(this.Rows))
-          q.PostAndReply(Msg.MsgNotifyMe)
 
+    interface System.IDisposable with
+        member this.Dispose() =
+            let q = this.Context.Provider.CommandQueue
+            q.Post(Msg.CreateFreeMsg<_>(this.Values))
+            q.Post(Msg.CreateFreeMsg<_>(this.Columns))
+            q.Post(Msg.CreateFreeMsg<_>(this.Rows))
+            q.PostAndReply(Msg.MsgNotifyMe)
