70db172fdaef1659d37fde06f5c0ef5312ccd999
kirillgarbar <st087492@student.spbu.ru>
2022-04-26 00:10:48 +0300

    Option tests

--- tests/GraphBLAS-sharp.Tests/BackendCommonTests/MatrixEwiseAddTests.fs
+++ tests/GraphBLAS-sharp.Tests/BackendCommonTests/MatrixEwiseAddTests.fs
@@ -7,8 +7,10 @@ open Expecto.Logging.Message
 open Brahma.FSharp.OpenCL
 open GraphBLAS.FSharp.Backend
 open GraphBLAS.FSharp
+open GraphBLAS.FSharp.Tests
 open GraphBLAS.FSharp.Tests.Utils
 open OpenCL.Net
+open StandardOperations
 
 let logger = Log.create "EwiseAdd.Tests"
 
@@ -29,13 +31,13 @@ let checkResult isEqual op zero (baseMtx1: 'a [,]) (baseMtx2: 'a [,]) (actual: M
     match actual with
     | MatrixCOO actual ->
         for i in 0 .. actual.Rows.Length - 1 do
+            if isEqual zero actual.Values.[i] then failwith "Resulting zeroes should be filtered."
             actual2D.[actual.Rows.[i], actual.Columns.[i]] <- actual.Values.[i]
-    | _ -> failwith "Impossible case."
-//    printfn "Actual \n %A" actual2D
-//    printfn "Expected \n %A" expected2D
+    | _ -> failwith "Resulting matrix should be converted to COO format."
+
     for i in 0 .. rows - 1 do
         for j in 0 .. columns - 1 do
-            Expect.isTrue (isEqual actual2D.[i, j] expected2D.[i, j]) "Values should be the same."
+            Expect.isTrue (isEqual actual2D.[i, j] expected2D.[i, j]) $"Values should be the same. Actual is {actual2D.[i, j]}, expected {expected2D.[i, j]}."
 
 let correctnessGenericTest
     zero
@@ -48,34 +50,7 @@ let correctnessGenericTest
     =
     let q = case.ClContext.CommandQueue
     q.Error.Add(fun e -> failwithf "%A" e)
-//    let leftMatrix = Array2D.zeroCreate 5 5
-//    let rightMatrix = Array2D.zeroCreate 5 5
-//    leftMatrix.[0, 1] <- 1
-//    leftMatrix.[1, 0] <- 10
-//    leftMatrix.[1, 1] <- 100
-//    leftMatrix.[1, 2] <- 1000
-//    leftMatrix.[2, 0] <- 10000
-//    rightMatrix.[0, 0] <- 2
-//    rightMatrix.[1, 0] <- 20
-//    rightMatrix.[1, 1] <- 200
-//    rightMatrix.[1, 3] <- 2000
-//    rightMatrix.[2, 2] <- 20000
-//    let mtx1 =
-//        { RowCount = 5
-//          ColumnCount = 5
-//          Rows = [|0; 1; 1; 1; 2|]
-//          Columns = [|1; 0; 1; 2; 0|]
-//          Values = [|1; 10; 100; 1000; 10000|]
-//    }
-//    let mtx1 = MatrixCOO mtx1
-//    let mtx2 =
-//        { RowCount = 5
-//          ColumnCount = 5
-//          Rows = [|0; 1; 1; 1; 2|]
-//          Columns = [|0; 0; 1; 3; 2|]
-//          Values = [|2; 20; 200; 2000; 20000|]
-//    }
-//    let mtx2 = MatrixCOO mtx2
+
     let mtx1 =
         createMatrixFromArray2D case.MatrixCase leftMatrix (isEqual zero)
 
@@ -111,21 +86,6 @@ let testFixtures case =
       let getCorrectnessTestName datatype =
           sprintf "Correctness on %s, %A, %A" datatype case (System.Random().Next())
 
-      let boolSum = <@
-          fun (x: bool option) (y: bool option) ->
-            let mutable res = false
-            match x, y with
-            | Some f, Some s -> res <- f || s
-            | Some f, None -> res <- f
-            | None, Some s -> res <- s
-            | None, None -> ()
-            if res = false then None else (Some res)
-      @>
-//      let intSum x y = <@
-//          fun (x: int option) (y: int option) ->
-//            match x with _ -> Some true
-//          @>
-
       let boolAdd =
           Matrix.eWiseAdd case.ClContext boolSum wgSize
 
@@ -135,34 +95,32 @@ let testFixtures case =
       |> correctnessGenericTest false (||) boolAdd boolToCOO (=)
       |> testPropertyWithConfig config (getCorrectnessTestName "bool")
 
-//      let intAdd =
-//          Matrix.eWiseAdd case.ClContext intSum wgSize
-//
-//      let intToCOO = Matrix.toCOO case.ClContext wgSize
-//
-//      case
-//      |> correctnessGenericTest 0 (+) intAdd intToCOO (=)
-//      |> testPropertyWithConfig config (getCorrectnessTestName "int")
+      let intAdd =
+          Matrix.eWiseAdd case.ClContext intSum wgSize
 
-//      let floatSum = sum (+)
-//      let floatAdd =
-//          Matrix.eWiseAdd case.ClContext <@ (+) @> wgSize
-//
-//      let floatToCOO = Matrix.toCOO case.ClContext wgSize
-//
-//      case
-//      |> correctnessGenericTest 0.0 (+) floatAdd floatToCOO (fun x y -> abs (x - y) < Accuracy.medium.absolute)
-//      |> testPropertyWithConfig config (getCorrectnessTestName "float")
+      let intToCOO = Matrix.toCOO case.ClContext wgSize
 
-//      let byteSum = sum (+)
-//      let byteAdd =
-//          Matrix.eWiseAdd case.ClContext <@ byteSum @> wgSize
-//
-//      let byteToCOO = Matrix.toCOO case.ClContext wgSize
-//
-//      case
-//      |> correctnessGenericTest 0uy (+) byteAdd byteToCOO (=)
-//      |> testPropertyWithConfig config (getCorrectnessTestName "byte")
+      case
+      |> correctnessGenericTest 0 (+) intAdd intToCOO (=)
+      |> testPropertyWithConfig config (getCorrectnessTestName "int")
+
+      let floatAdd =
+          Matrix.eWiseAdd case.ClContext floatSum wgSize
+
+      let floatToCOO = Matrix.toCOO case.ClContext wgSize
+
+      case
+      |> correctnessGenericTest 0.0 (+) floatAdd floatToCOO (fun x y -> abs (x - y) < Accuracy.medium.absolute)
+      |> testPropertyWithConfig config (getCorrectnessTestName "float")
+
+      let byteAdd =
+          Matrix.eWiseAdd case.ClContext byteSum  wgSize
+
+      let byteToCOO = Matrix.toCOO case.ClContext wgSize
+
+      case
+      |> correctnessGenericTest 0uy (+) byteAdd byteToCOO (=)
+      |> testPropertyWithConfig config (getCorrectnessTestName "byte")
       ]
 
 let tests =
--- tests/GraphBLAS-sharp.Tests/Helpers.fs
+++ tests/GraphBLAS-sharp.Tests/Helpers.fs
@@ -448,3 +448,39 @@ module Utils =
     let createVectorFromArray vectorCase array isZero =
         match vectorCase with
         | VectorFormat.COO -> VectorCOO <| COOVector.FromArray(array, isZero)
+
+module StandardOperations =
+    let boolSum = <@ fun (_: bool option) (_: bool option) -> Some true @>
+
+    let intSum = <@
+        fun (x: int option) (y: int option) ->
+        let mutable res = 0
+        match x, y with
+        | Some f, Some s -> res <- f + s
+        | Some f, None -> res <- f
+        | None, Some s -> res <- s
+        | None, None -> ()
+        if res = 0 then None else (Some res)
+    @>
+
+    let byteSum = <@
+        fun (x: byte option) (y: byte option) ->
+        let mutable res = 0uy
+        match x, y with
+        | Some f, Some s -> res <- f + s
+        | Some f, None -> res <- f
+        | None, Some s -> res <- s
+        | None, None -> ()
+        if res = 0uy then None else (Some res)
+    @>
+
+    let floatSum = <@
+        fun (x: float option) (y: float option) ->
+        let mutable res = 0.0
+        match x, y with
+        | Some f, Some s -> res <- f + s
+        | Some f, None -> res <- f
+        | None, Some s -> res <- s
+        | None, None -> ()
+        if abs(res) < 1E-08 then None else (Some res)
+    @>
