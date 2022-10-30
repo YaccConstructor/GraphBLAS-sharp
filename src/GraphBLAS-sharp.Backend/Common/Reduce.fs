3eb9c52883f4ab343be196405a0741075b45c249
IgorErin <erin.igore@yandex.ru>
2022-10-31 00:22:46 +0300

    add: dot in index

--- src/GraphBLAS-sharp.Backend/Common/Reduce.fs
+++ src/GraphBLAS-sharp.Backend/Common/Reduce.fs
@@ -24,7 +24,7 @@ module Reduce =
                     let i = (gid - lid) * 2 + lid
 
                     if i + workGroupSize < length then
-                        localValues[lid] <- ((%opAdd) inputArray[i] inputArray[i + workGroupSize])
+                        localValues.[lid] <- (%opAdd) inputArray.[i] inputArray.[i + workGroupSize]
                     elif i < length then
                         localValues[lid] <- inputArray[i]
                     else
@@ -89,7 +89,7 @@ module Reduce =
                     let i = (gid - lid) * 2 + lid
 
                     if i + workGroupSize < length then
-                        localValues[lid] <- ((%opAdd) inputArray[i] inputArray[i + workGroupSize])
+                        localValues[lid] <- (%opAdd) inputArray.[i] inputArray.[i + workGroupSize]
                     elif i < length then
                         localValues[lid] <- inputArray[i]
                     else
@@ -235,7 +235,7 @@ module Reduce =
                         barrierLocal ()
 
                     if lid = 0 then
-                       (atomic %opAdd totalSum.Value localValues[0]) |> ignore
+                       atomic %opAdd totalSum.Value localValues.[0] |> ignore
             @>
 
         let kernel = clContext.Compile(reduce)
