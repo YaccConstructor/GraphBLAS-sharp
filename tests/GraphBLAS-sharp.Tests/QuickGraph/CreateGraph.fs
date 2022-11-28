dfe085d8edecaddfaf8dd0b626f8ddc08f542ad5
kirillgarbar <st087492@student.spbu.ru>
2022-11-28 04:23:14 +0300

    QuickGraph methods and algorithms

---
+++ tests/GraphBLAS-sharp.Tests/QuickGraph/CreateGraph.fs
@@ -0,0 +1,37 @@
+﻿namespace GraphBLAS.FSharp.Tests.QuickGraph
+
+open QuickGraph
+
+module CreateGraph =
+    let directedFromArray2D (matrix: 'a [,]) zero =
+        let graph = AdjacencyGraph<'a, Edge<'a>>()
+
+        for outVertex in 0 .. Array2D.length1 matrix - 1 do
+            for inVertex in 0 .. Array2D.length2 matrix - 1 do
+                if matrix.[outVertex, inVertex] <> zero then
+                    graph.AddVerticesAndEdge(new Edge<'a>(outVertex, inVertex))
+                    |> ignore
+
+        graph
+
+    let undirectedFromArray2D (matrix: 'a [,]) zero =
+        let edgeMatrix =
+            Array2D.mapi
+                (fun r c v ->
+                    if v <> zero then
+                        Some(Edge<'a>(r, c))
+                    else
+                        None)
+                matrix
+
+        let edgeList =
+            edgeMatrix
+            |> Seq.cast<Edge<'a> option>
+            |> Seq.fold
+                (fun l x ->
+                    match x with
+                    | Some x -> x :: l
+                    | _ -> l)
+                []
+
+        edgeList.ToUndirectedGraph()
