e1a0ae57bc9987eedbaa7c876dbf3a0d7eafdd47
github-action-benchmark <github@users.noreply.github.com>
2023-03-06 23:09:32 +0300

    add BFS (benchmarkdotnet) benchmark result for 6a0b88c13ad6bfdb8b49f271641ef33a9194e449

---
+++ docs/benchmarks/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1678133371981,
+  "repoUrl": "https://github.com/kirillgarbar/GraphBLAS-sharp",
+  "entries": {
+    "BFS": [
+      {
+        "commit": {
+          "author": {
+            "email": "71129570+kirillgarbar@users.noreply.github.com",
+            "name": "Kirill",
+            "username": "kirillgarbar"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6a0b88c13ad6bfdb8b49f271641ef33a9194e449",
+          "message": "Merge branch 'dev' into bfs",
+          "timestamp": "2023-03-06T22:39:01+03:00",
+          "tree_id": "6ac1e3c2895b2d212448bce7c538abe10e9b0eb9",
+          "url": "https://github.com/kirillgarbar/GraphBLAS-sharp/commit/6a0b88c13ad6bfdb8b49f271641ef33a9194e449"
+        },
+        "date": 1678133371873,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "coAuthorsCiteseer.mtx",
+            "value": 26976326.1,
+            "unit": "ns",
+            "range": "± 1188416.3809004356"
+          },
+          {
+            "name": "hollywood-2009.mtx",
+            "value": 244664916.75,
+            "unit": "ns",
+            "range": "± 427244.22641279944"
+          },
+          {
+            "name": "roadNet-CA.mtx",
+            "value": 276640576.8,
+            "unit": "ns",
+            "range": "± 856227.0199268674"
+          },
+          {
+            "name": "wing.mtx",
+            "value": 81482273.9,
+            "unit": "ns",
+            "range": "± 7462531.223758084"
+          }
+        ]
+      }
+    ]
+  }
+}
