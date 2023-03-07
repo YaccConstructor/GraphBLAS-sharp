2a636b153e6180e54a7f3d92c46bad6a2029247b
github-action-benchmark <github@users.noreply.github.com>
2023-03-07 09:43:02 +0300

    add BFS (benchmarkdotnet) benchmark result for ee8129199ea4b34978b1717f07eeeca0b8f5a97c

--- docs/benchmarks/data.js
+++ docs/benchmarks/data.js
@@ -1,6 +1,6 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678133371981,
-  "repoUrl": "https://github.com/kirillgarbar/GraphBLAS-sharp",
+  "lastUpdate": 1678171382517,
+  "repoUrl": "https://github.com/YaccConstructor/GraphBLAS-sharp",
   "entries": {
     "BFS": [
       {
@@ -48,6 +48,54 @@ window.BENCHMARK_DATA = {
             "value": 81482273.9,
             "unit": "ns",
             "range": "± 7462531.223758084"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "st087492@student.spbu.ru",
+            "name": "kirillgarbar",
+            "username": "kirillgarbar"
+          },
+          "committer": {
+            "email": "st087492@student.spbu.ru",
+            "name": "kirillgarbar",
+            "username": "kirillgarbar"
+          },
+          "distinct": true,
+          "id": "ee8129199ea4b34978b1717f07eeeca0b8f5a97c",
+          "message": "Restore tools before build",
+          "timestamp": "2023-03-07T02:25:51+03:00",
+          "tree_id": "724cf3d61852fb6dc8b8e255a536264f4c813726",
+          "url": "https://github.com/YaccConstructor/GraphBLAS-sharp/commit/ee8129199ea4b34978b1717f07eeeca0b8f5a97c"
+        },
+        "date": 1678171382164,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "coAuthorsCiteseer.mtx",
+            "value": 27438076.7,
+            "unit": "ns",
+            "range": "± 1297181.9540538252"
+          },
+          {
+            "name": "hollywood-2009.mtx",
+            "value": 243953769.44444445,
+            "unit": "ns",
+            "range": "± 369898.03864481056"
+          },
+          {
+            "name": "roadNet-CA.mtx",
+            "value": 386331568.3,
+            "unit": "ns",
+            "range": "± 547459.559564205"
+          },
+          {
+            "name": "wing.mtx",
+            "value": 74053240.7,
+            "unit": "ns",
+            "range": "± 6156789.947659613"
           }
         ]
       }
