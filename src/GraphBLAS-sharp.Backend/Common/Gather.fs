d55d34f7b6b5685a84245b48d1cc648560520e97
IgorErin <erin.igore@yandex.ru>
2023-03-12 16:15:05 +0300

    add: Gather

---
+++ src/GraphBLAS-sharp.Backend/Common/Gather.fs
@@ -0,0 +1,41 @@
+namespace GraphBLAS.FSharp.Backend.Common.Gather
+
+open Brahma.FSharp
+
+module internal Gather =
+    /// <summary>
+    /// Creates a new array obtained from positions replaced with values from the given array at these positions (indices).
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// let positions = [| 2; 0; 2; 1 |]
+    /// let array = [| 1.4; 2.5; 3.6 |]
+    /// ...
+    /// > val result = [| 3.6; 1.4; 3.6; 2.5 |]
+    /// </code>
+    /// </example>
+    let run (clContext: ClContext) workGroupSize =
+
+        let gather =
+            <@ fun (ndRange: Range1D) (positions: ClArray<int>) (inputArray: ClArray<'a>) (outputArray: ClArray<'a>) (size: int) ->
+
+                let i = ndRange.GlobalID0
+
+                if i < size then
+                    outputArray.[i] <- inputArray.[positions.[i]] @>
+
+        let program = clContext.Compile(gather)
+
+        fun (processor: MailboxProcessor<_>) (positions: ClArray<int>) (inputArray: ClArray<'a>) (outputArray: ClArray<'a>) ->
+
+            let size = outputArray.Length
+
+            let kernel = program.GetKernel()
+
+            let ndRange = Range1D.CreateValid(size, workGroupSize)
+
+            processor.Post(
+                Msg.MsgSetArguments(fun () -> kernel.KernelFunc ndRange positions inputArray outputArray size)
+            )
+
+            processor.Post(Msg.CreateRunMsg<_, _>(kernel))
