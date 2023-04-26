2b4a5b79ce423aa65e4d30627e5046ee8dac0bcd
IgorErin <erin.igore@yandex.ru>
2023-04-26 13:34:47 +0300

    add: Vector.Merge

---
+++ tests/GraphBLAS-sharp.Tests/Common/Merge.fs
@@ -0,0 +1,83 @@
+module GraphBLAS.FSharp.Tests.Common.Merge
+
+open GraphBLAS.FSharp.Backend.Vector
+open GraphBLAS.FSharp.Backend.Common
+open GraphBLAS.FSharp.Objects
+open GraphBLAS.FSharp.Tests
+open GraphBLAS.FSharp.Backend.Objects.ArraysExtensions
+open Brahma.FSharp
+open Expecto
+
+let processor = Context.defaultContext.Queue
+
+let context = Context.defaultContext.ClContext
+
+type Result<'a>= None | Left of 'a | Right of 'a
+
+let config = { Utils.defaultConfig with endSize = 100000 }
+
+let makeTest isEqual zero testFun (firstArray: 'a []) (secondArray: 'a []) =
+    let firstVector = Vector.Sparse.FromArray(firstArray, isEqual zero)
+
+    let secondVector = Vector.Sparse.FromArray(secondArray, isEqual zero)
+
+    if firstVector.NNZ > 0 && secondVector.NNZ > 0 then
+
+        // actual run
+        let clFirstVector = firstVector.ToDevice context
+
+        let clSecondVector = secondVector.ToDevice context
+
+        let (allIndices: ClArray<int>), (firstValues: ClArray<'a>), (secondValues: ClArray<'a>), (isLeftBitmap: ClArray<int>) =
+            testFun processor clFirstVector clSecondVector
+
+        clFirstVector.Dispose processor
+        clSecondVector.Dispose processor
+
+        let actualIndices = allIndices.ToHostAndFree processor
+        let actualFirstValues = firstValues.ToHostAndFree processor
+        let actualSecondValues = secondValues.ToHostAndFree processor
+        let actualIsLeftBitmap = isLeftBitmap.ToHostAndFree processor
+
+        let actualValues =
+            (actualFirstValues, actualSecondValues, actualIsLeftBitmap)
+            |||> Array.map3 (fun leftValue rightValue isLeft -> if isLeft = 1 then leftValue else rightValue)
+
+        // expected run
+        let firstValuesAndIndices =
+            Array.map2 (fun value index -> (value, index)) firstVector.Values firstVector.Indices
+
+        let secondValuesAndIndices =
+            Array.map2 (fun value index -> (value, index)) secondVector.Values secondVector.Indices
+
+        // preserve order of values then use stable sort
+        let allValuesAndIndices =
+            Array.concat [ firstValuesAndIndices; secondValuesAndIndices ]
+
+        // stable sort
+        let expectedValues, expectedIndices =
+            Seq.sortBy snd allValuesAndIndices
+            |> Seq.toArray
+            |> Array.unzip
+
+        "Values should be the same"
+        |> Utils.compareArrays isEqual actualValues expectedValues
+
+        "Indices should be the same"
+        |> Utils.compareArrays (=) actualIndices expectedIndices
+
+let createTest<'a when 'a : struct> isEqual (zero: 'a) =
+    Merge.Vector.run context Utils.defaultWorkGroupSize
+    |> makeTest isEqual zero
+    |> testPropertyWithConfig config $"test on %A{typeof<'a>}"
+
+let tests =
+    [ createTest<int> (=) 0
+
+      if Utils.isFloat64Available context.ClDevice then
+        createTest<float> (=) 0.0
+
+      createTest<float32> Utils.float32IsEqual 0.0f
+      createTest<bool> (=) false ]
+    |> testList "Merge"
+
