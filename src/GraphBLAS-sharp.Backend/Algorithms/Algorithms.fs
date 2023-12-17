082656281c1397217916574ccfbf53899eeff778
kirillgarbar <st087492@student.spbu.ru>
2023-12-17 19:24:12 +0300

    Docs for PageRank

--- src/GraphBLAS-sharp.Backend/Algorithms/Algorithms.fs
+++ src/GraphBLAS-sharp.Backend/Algorithms/Algorithms.fs
@@ -21,6 +21,22 @@ module Algorithms =
         let run = SSSP.run
 
     module PageRank =
+        /// <summary>
+        /// Computes PageRank of the given matrix.
+        /// Matrix should be prepared in advance using "PageRank.prepareMatrix" method.
+        /// Accepts accuracy as a parameter which determines how many iterations will be performed.
+        /// Values of accuracy lower than 1e-06 are not recommended since the process may never stop.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// let preparedMatrix = PageRank.prepareMatrix clContext workGroupSize queue matrix
+        /// let accuracy = 1e-05
+        /// let pageRank = PageRank.run clContext workGroupSize queue preparedMatrix accuracy
+        /// </code>
+        /// </example>
         let run = PageRank.run
 
+        /// <summary>
+        /// Converts matrix representing a graph to a format suitable for PageRank algorithm.
+        /// </summary>
         let prepareMatrix = PageRank.prepareMatrix
